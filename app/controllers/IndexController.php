078a24a6f544ddb68d4ad91f00990b12a62fbde8
Eugene Gubar <eugene.gubar@gmail.com>
2019-09-18 13:46:01 +0300

    Begin app

---
+++ app/controllers/IndexController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace controllers;
+
+
+/**
+ * Controller IndexController
+ **/
+class IndexController extends ControllerBase
+{
+
+    public function index()
+    {
+        
+    }
+    
+}
