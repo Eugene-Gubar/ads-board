d5958ebf87d3efabeed4c325fce2eb41b7fb5209
Eugene Gubar <eugene.gubar@gmail.com>
2019-09-30 16:52:03 +0300

    fix query condition

--- app/controllers/IndexController.php
+++ app/controllers/IndexController.php
@@ -37,7 +37,7 @@ class IndexController extends ControllerBase
     {
         if ($pageNum > 0 && $rowsPerPage > 0) {
 
-            $ads = DAO::paginate(Ads::class, $pageNum, $rowsPerPage, 'ORDER BY id DESC');
+            $ads = DAO::paginate(Ads::class, $pageNum, $rowsPerPage, "order by id desc");
             $count = DAO::count(Ads::class);
             $pages = ceil($count / $rowsPerPage);
 
