00ca781f506d5911bb309b6650b340bf086bda0f
Eugene Gubar <eugene.gubar@gmail.com>
2019-10-02 15:03:34 +0300

    check session

--- app/controllers/UserDashboardController.php
+++ app/controllers/UserDashboardController.php
@@ -94,6 +94,7 @@ class UserDashboardController extends ControllerBase
                 if (is_array($violations) && sizeof($violations) === 0) {
 
                     $user = DAO::uGetOne(User::class, USession::get('activeUser')->getId(), false);
+                    echo '<pre>'.USession::get('activeUser')->getId().'</pre>';
                     $adv->setUser($user);
                     $adv->setTs(date('Y-m-d H:i:s'));
 
